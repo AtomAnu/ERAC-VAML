a64989b7164b82fb53634b254d8fe6fbae03590f
AtomAnu <a.anuchitanukul@gmail.com>
2021-04-08 13:52:44 +0700

    Updated train_erac.py

--- mt/erac/train_erac.py
+++ mt/erac/train_erac.py
@@ -230,8 +230,8 @@ def train(epoch):
 
         # optimization
         act_optimizer.zero_grad()
-        print((loss_act + args.mle_coeff * loss_mle).device)
-        (loss_act + args.mle_coeff * loss_mle).backward()
+        loss_act.backward()
+        # (loss_act + args.mle_coeff * loss_mle).backward()
         gnorm_act = nn.utils.clip_grad_norm(actor.parameters(), args.grad_clip)
         act_optimizer.step()
 
