497ca9b406b673de8430a7b90bcf98bf611dcd78
AtomAnu <a.anuchitanukul@gmail.com>
2021-04-08 11:44:47 +0700

    Updated data.py

--- shared/data.py
+++ shared/data.py
@@ -56,8 +56,7 @@ class BucketIterator(object):
             batch = batch.cuda()
         
         if self.variable:
-            print(batch.dtype)
-            batch = Variable(batch, requires_grad=self.volatile)
+            batch = Variable(batch.float(), requires_grad=self.volatile)
 
         return batch
 
