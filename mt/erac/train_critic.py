3438dd091c576c1acedaa060db8410ae8972b416
AtomAnu <a.anuchitanukul@gmail.com>
2021-04-24 12:52:20 +0700

    Updated train_critic.py

--- mt/erac/train_critic.py
+++ mt/erac/train_critic.py
@@ -192,11 +192,13 @@ def train(epoch):
         # compute rewards
         ref, hyp = utils.prepare_for_bleu(tgt, seq, eos_idx=eos_idx, pad_idx=tgt_pad_idx, unk_idx=tgt_unk_idx)
         bleu_R, score = utils.get_rewards(bleu_metric, hyp, ref, return_bleu=True)
-
+        print('BLEU Reward Shape: {}'.format(bleu_R.size()))
+        print('Hyp Shape: {}'.format(hyp.size()))
         if args.use_unsuper_reward:
             R = utils.get_unsuper_rewards(GPTLM, tokenizer, XLM, bpe, dico, params, cos_sim, vocab, src, hyp,
                                           inc_adequacy=args.include_adequacy, mu=args.mu)
             R = R.to('cuda')
+            print('Unsuper R Shape: {}'.format(R.size()))
         else:
             R = bleu_R
 
