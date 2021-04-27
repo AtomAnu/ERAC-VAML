fc1f68ea5c8bc95f50c58a1827f5c9a1582e8aa7
AtomAnu <a.anuchitanukul@gmail.com>
2021-04-27 20:11:32 +0700

    Updated test_fluency.py

--- test_fluency.py
+++ test_fluency.py
@@ -2,7 +2,8 @@ import torch
 # from pytorch_pretrained_bert import BertTokenizer, BertForMaskedLM
 from pytorch_pretrained_bert import OpenAIGPTTokenizer, OpenAIGPTModel, OpenAIGPTLMHeadModel
 import math
-
+import time
+import html
 # bertMaskedLM = BertForMaskedLM.from_pretrained('bert-base-uncased')
 # bertMaskedLM.eval()
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
@@ -34,16 +35,76 @@ model.eval()
 # Load pre-trained model tokenizer (vocabulary)
 tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
 
-def score(sentence):
+def calculate_fluency(sentence):
+    sentence = html.unescape(sentence)
     tokenize_input = tokenizer.tokenize(sentence)
     tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
     loss=model(tensor_input, lm_labels=tensor_input)
+
     return math.exp(loss)
 
 
-sample_inputs = ['I am happy',
-                 'I just want to go to sleep',
-                 'I just want to go to cow',
-                 'Cow animal cat nugget',
-                 'Hello World']
-print([score(i) for i in sample_inputs])
+# sample_inputs = ["I'm happy",
+#                  "I &apos;m happy",
+#                  'I just want to go to sleep',
+#                  'I just want to go to cow',
+#                  'Cow animal cat nugget',
+#                  'Hello World']
+#
+# sample_inputs = ["I 'd like to say that you 're very special",
+#                  "I &apos;d like to say that you &apos;re very special"]
+
+# sample = "I &apos;d like to say that you &apos;re very special"
+sample = 'i had a voyage to tens of thousands of of canopy attacks , ' \
+         'one place that went , was a little bit more , didn &apos;t have that small typing ' \
+         '-- that tube -- had caught all 200 feet in the little fish .'
+
+################ Current Implementation ######################
+
+start = time.time()
+
+sample_words = sample.strip().lower().split()
+
+fluency_scores = []
+
+for i in range(1, len(sample_words)):
+
+    sent = ' '.join([word for word in sample_words[:i+1]])
+    fluency_scores.append(calculate_fluency(sent))
+
+end = time.time()
+print(end-start)
+
+print(fluency_scores)
+
+################ New Implementation ######################
+
+# sample = "I &apos;d like to say that you &apos;re very special"
+sample = 'i had a voyage to tens of thousands of of canopy attacks , ' \
+         'one place that went , was a little bit more , didn &apos;t have that small typing ' \
+         '-- that tube -- had caught all 200 feet in the little fish .'
+
+def calculate_fluency(tensor_input):
+
+    loss=model(tensor_input, lm_labels=tensor_input)
+
+    return math.exp(loss)
+
+start = time.time()
+
+sample = html.unescape(sample)
+sample_words = sample.strip().lower().split()
+
+sent_ids = tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sample_words[0]))
+fluency_scores = []
+
+for i in range(1, len(sample_words)):
+
+    sent_ids += tokenizer.convert_tokens_to_ids(tokenizer.tokenize(sample_words[i]))
+    tensor_input = torch.tensor([sent_ids])
+    fluency_scores.append(calculate_fluency(tensor_input))
+
+end = time.time()
+print(end-start)
+
+print(fluency_scores)
