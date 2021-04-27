5be6cab93a2ceba20e99e1a29d03fd8ca550e5e5
AtomAnu <a.anuchitanukul@gmail.com>
2021-04-27 20:59:44 +0700

    Optimised fluency calculation

--- shared/utils.py
+++ shared/utils.py
@@ -184,7 +184,7 @@ def calculate_fluency(lm, tokenizer, hyp_sent):
     with torch.no_grad():
         tokenize_input = tokenizer.tokenize(hyp_sent)
         if len(tokenize_input) == 0: return 0
-        tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)]).to(device)
+        tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)]).to('cuda')
         loss = lm(tensor_input, lm_labels=tensor_input)
         fluency = 1/np.exp(loss.item())
     return fluency
