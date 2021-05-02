3a15709aaccc15cb8d5a9d819cf7e60e07ab95c0
AtomAnu <a.anuchitanukul@gmail.com>
2021-05-02 23:01:56 +0700

    Added a second fluency test

--- test_fluency_2.py
+++ test_fluency_2.py
@@ -1,10 +1,10 @@
 import torch
-from pytorch_pretrained_bert import OpenAIGPTTokenizer, OpenAIGPTLMHeadModel
+from transformers import OpenAIGPTTokenizer, OpenAIGPTLMHeadModel
 
 tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
 model = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt')
 
-inputs = tokenizer.tokenize("Hello, my dog is cute", return_tensors="pt")
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
 outputs = model(**inputs, labels=inputs["input_ids"])
 loss = outputs.loss
 logits = outputs.logits
