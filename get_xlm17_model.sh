bdd0271b21f808dd98302ecae18a8b02d2488638
AtomAnu <a.anuchitanukul@gmail.com>
2021-05-08 00:43:43 +0700

    Test files for XLM 17

---
+++ get_xlm17_model.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+wget https://dl.fbaipublicfiles.com/XLM/mlm_17_1280.pth
+wget https://dl.fbaipublicfiles.com/XLM/codes_xnli_17
+wget https://dl.fbaipublicfiles.com/XLM/vocab_xnli_17
